32380766ef5e4b2d184fcd3dd2d943cb26ee08ab
madhav411 <44801760+madhav411@users.noreply.github.com>
2022-09-18 14:07:43 +0530

    refactored the script

--- arith_if.sh
+++ arith_if.sh
@@ -1,43 +1,34 @@
+#!/bin/bash
 echo "Please select any one of the number to perform arithmetic operations:"
 read var
+
+function enter_the_values() {
+  echo Please enter the 1st digit:
+  read x
+  echo please enter the 2nd digit:
+  read y
+}
+
 if [ $var == 1 ];
 then
- echo please enter the 1st digit:
- read x
- echo please enter the 2nd digit:
- read y
- echo The addition of given numbers are:`expr $x + $y`
+  enter_the_values
+  echo "The addition of given numbers are:`expr $x + $y`"
 elif [ $var == 2 ];
 then
- echo please enter the 1st digit:
- read x
- echo please enter the 2nd digit:
- read y
- echo The Subtraction of given numbers are:`expr $x - $y`
+  enter_the_values
+  echo "The Subtraction of given numbers are:`expr $x - $y`"
 elif [ $var == 3 ];
 then
- echo please enter the 1st digit:
- read x
- echo please enter the 2nd digit:
- read y
- echo The Multiplication of given numbers are:`expr $x \* $y`
+  enter_the_values
+  echo "The Multiplication of given numbers are:`expr $x \* $y`"
 elif [ $var == 4 ];
 then
- echo please enter the 1st digit:
- read x
- echo please enter the 2nd digit:
- read y
- echo The Subtraction of given numbers are:`expr $x / $y`
+  enter_the_values
+  echo "The Subtraction of given numbers are:`expr $x / $y`"
 elif [ $var == 5 ];
 then
- echo please enter the 1st digit:
- read x
- echo please enter the 2nd digit:
- read y
- echo The Division of given numbers are:`expr $x % $y`
+  enter_the_values
+  echo "The Division of given numbers are:`expr $x % $y`"
 else
- echo "The entered number is not a valid arithmetic operation"
+  echo 'Please enter the numeric values !!'
 fi
-
-
-
